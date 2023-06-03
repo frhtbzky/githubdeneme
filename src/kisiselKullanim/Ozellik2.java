e38a49df6b5c5d8a31f7bc9bfa415bd0f5ff83fb
frhtbzky <ferhat.sunshine01@gmail.com>
2023-06-03 14:43:28 +0300

    ahmet branchi gucellendi

--- src/kisiselKullanim/Ozellik2.java
+++
@@ -1,7 +0,0 @@
-package kisiselKullanim;
-
-public class Ozellik2 {
-    public static void main(String[] args) {
-        System.out.println("Nihat bunu yazdi");
-    }
-}
--- src/kisiselKullanim/ozellik1.java
+++ src/kisiselKullanim/ozellik1.java
@@ -1,9 +1,9 @@
 package kisiselKullanim;
 
 public class ozellik1 {
+
     public static void main(String[] args) {
 
         System.out.println("Ahmet branch'inde calisiyorum");
-        System.out.println("Ahmet branchinde 2.degisiklik kendime ozel class'da");
     }
 }
